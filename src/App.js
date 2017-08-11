2ec82471bee1229d373dc1305f69fdfc24bba057
Antoine Markarian <am@getkello.com>
2017-08-11 11:54:20 +0800

    search now uses routes instead of state

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Bookshelf from './Components/Bookshelf'
 import Search from './Components/Search'
-import ReactRouter from 'react-router-dom';
+import { Route, Link } from 'react-router-dom'
 
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: []
   }
 
@@ -28,30 +21,27 @@ class BooksApp extends React.Component {
     const allBooks = this.state.books
     return (
       <div className="app">
+          <Route exact path='/' render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>My Reads</h1>
+              </div>
+              <div className="list-books-content">
+                  {!allBooks.length ? <p className="loading-message">Fetching your books...</p> :
+                  <div>
+                      <Bookshelf title='Currently reading' ukey='currentlyReading' books={allBooks} />
+                      <Bookshelf title='Want to read' ukey='wantToRead' books={allBooks} />
+                      <Bookshelf title='Read' ukey='read' books={allBooks} />
+                  </div>
+                  }
 
-
-        {this.state.showSearchPage ? (
-          <Search />
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>My Reads</h1>
+              </div>
+              <div className="open-search">
+                <Link to='/search'>Add a book</Link>
+              </div>
             </div>
-            <div className="list-books-content">
-                {!allBooks.length ? <p className="loading-message">Fetching your books...</p> :
-                <div>
-                    <Bookshelf title='Currently reading' ukey='currentlyReading' books={allBooks} />
-                    <Bookshelf title='Want to read' ukey='wantToRead' books={allBooks} />
-                    <Bookshelf title='Read' ukey='read' books={allBooks} />
-                </div>
-                }
-
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+          )}/>
+          <Route path='/search' component={Search} />
       </div>
     )
   }
